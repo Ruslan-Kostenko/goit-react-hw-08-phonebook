3e1fdc5bbfd72d26de165ed1778c368aed856ba2
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-10-20 22:50:35 +0000

    Deploying to gh-pages from @ Ruslan-Kostenko/goit-react-hw-08-phonebook@02df743ff942935065a39f6617073d8213a72559 🚀

---
+++ static/js/791.f17680cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[791],{6791:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var r=n(4270),i=n(184);function s(){return(0,i.jsxs)("main",{children:[(0,i.jsx)(r.q,{title:"Not Found..."}),(0,i.jsxs)("div",{children:[(0,i.jsx)("h1",{style:{fontSize:120},children:"404"}),(0,i.jsx)("b",{style:{fontSize:28},children:"Sorry, we couldn't find that page..."})]})]})}}}]);
+//# sourceMappingURL=791.f17680cb.chunk.js.map
